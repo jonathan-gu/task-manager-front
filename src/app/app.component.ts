f1b4368d1e3719eb946732514b7f38df7e363a64
jonathan <jonathanguerpillon@gmail.com>
2024-02-27 23:34:01 +0100

    add toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [
+    MatToolbarModule
+  ]
 })
+
 export class AppComponent {
-  title = 'task-manager-front';
 }
